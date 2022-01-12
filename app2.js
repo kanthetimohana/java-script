87eddf2138ae17fbb4fbbde956b398bfcc16e245
kanthetimohana <65145167+kanthetimohana@users.noreply.github.com>
2022-01-12 12:34:47 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,10 @@
+let a=54;
+let b=65;
+if(a>b)
+{
+    console.log("a is greater than b");
+}
+else
+{
+    console.log("b is greater than a");
+}
---
+++ reference.js
@@ -0,0 +1,5 @@
+const m=[1,2,3,4,5]
+let a=m
+m.push(6)
+console.log(m);
+console.log(a);
